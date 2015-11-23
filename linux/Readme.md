1e30df4891c343ca857e549653334a65720826a1
Stephen <liwp.stephen@gmail.com>
2015-11-23 10:26:40 +0800

    add secure server script

---
+++ linux/Readme.md
@@ -0,0 +1,9 @@
+# The script for Linux
+
+# Secure server
+
+The linux server will save history on the file system, it is not secure if this folder accessed by others.
+
+The secure_server.sh file will link the history files to /dev/null
+
+
---
+++ linux/secure_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm ~/.bash_history && ln -s /dev/null ~/.bash_history
+rm ~/.mysql_history && ln -s /dev/null ~/.mysql_history
+rm ~/.psql_history && ln -s /dev/null ~/.psql_history
+rm ~/.sqlite_history && ln -s /dev/null ~/.mysql_history
+
