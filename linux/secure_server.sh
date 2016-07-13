ba4712d25da26e7387c48c12392caf43b0814746
Stephen <liwp.stephen@gmail.com>
2016-07-13 16:27:18 -0500

    Change link command
    Use function to link files.

--- linux/secure_server.sh
+++ linux/secure_server.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-rm ~/.bash_history && ln -s /dev/null ~/.bash_history
-rm ~/.mysql_history && ln -s /dev/null ~/.mysql_history
-rm ~/.psql_history && ln -s /dev/null ~/.psql_history
-rm ~/.sqlite_history && ln -s /dev/null ~/.mysql_history
+link_file_dev_null() {
+	if [ -z "$1" ]; then
+		echo "-Parameter file is empty, please input the file path to link with /dev/null"
+	else
+		echo "Link file $1 to /dev/null"
+		[ -e $1 ] && rm $1
+		ln -s /dev/null $1
+	fi
+}
 
+link_file_dev_null ~/.bash_history
+link_file_dev_null ~/.mysql_history
+link_file_dev_null ~/.psql_history
+link_file_dev_null ~/.sqlite_history
