47e088dd7587b584458f254ab671398e81ed2f78
Stephen <liwp.stephen@gmail.com>
2017-06-07 10:55:09 +0800

    change to 10.2

--- db/install_mariadb.sh
+++ db/install_mariadb.sh
@@ -1,9 +1,9 @@
 touch /etc/yum.repos.d/MariaDB.repo
-echo '# MariaDB 10.0 CentOS repository list - created 2015-06-02 07:26 UTC
-# http://mariadb.org/mariadb/repositories/
+echo '# MariaDB 10.2 CentOS repository list - created 2017-06-07 02:50 UTC
+# http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.0/centos7-amd64
+baseurl = http://yum.mariadb.org/10.2/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1' > /etc/yum.repos.d/MariaDB.repo
 yum install -y MariaDB-server MariaDB-client
