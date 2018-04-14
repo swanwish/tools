c6ed99b14f3dc465947c2c6cedb5b4e116af66f5
Stephen <liwp.stephen@gmail.com>
2018-04-14 16:37:14 -0700

    fix restart_service.sh

--- linux/restart_service.sh
+++ linux/restart_service.sh
@@ -6,30 +6,32 @@
 serviceBackupDir=.service_backup
 
 if [ -z "$1" ]; then
-	echo Please pass the service name
-	exit
+    echo Please pass the service name
+    exit
 else
-	SERVICE_NAME=$1
+    SERVICE_NAME=$1
 fi
 
 if [ ! $2 ]; then
-	echo Set service dir as ~/servers
-	SERVICE_DIR=~/servers
+    echo Set service dir as ~/servers
+    SERVICE_DIR=~/servers
 else
-	SERVICE_DIR=$2
+    SERVICE_DIR=$2
 fi
 
 if [ ! $3 ]; then
-	echo Service param is not specified.
-	SERVICE_PARAM=""
+    echo Service param is not specified.
+    SERVICE_PARAM=""
+    CHECK_SERVICE_NAME="$SERVICE_NAME$"
 else
-	SERVICE_PARAM="$3"
+    SERVICE_PARAM="$3"
+    CHECK_SERVICE_NAME="$SERVICE_NAME "
 fi
 
-pid=`ps -aux | grep "$SERVICE_NAME " | grep -v grep | grep -v "$0" | awk '{print $2}'`
+pid=`ps -aux | grep "$CHECK_SERVICE_NAME" | grep -v grep | grep -v "$0" | awk '{print $2}'`
 if [ ! -z "$pid" ]; then
-	echo "Stop service $SERVICE_NAME"
-	kill -9 $pid
+    echo "Stop service $SERVICE_NAME"
+    kill -9 $pid
 fi
 
 cd $SERVICE_DIR
@@ -38,21 +40,21 @@ ZIP_FILE_NAME=$SERVICE_NAME.tar.gz
 
 if [ -f $ZIP_FILE_NAME ]
     then
-		if [ -d $SERVICE_NAME ]
-		then
-			echo Backup service
-      [[ -d $SERVICE_DIR/$serviceBackupDir ]] || mkdir $SERVICE_DIR/$serviceBackupDir
-			create_time=`date -r $SERVICE_NAME +"%Y%m%d_%H%M%S"`
-			tar czf $serviceBackupDir/"$SERVICE_NAME"_"$create_time".tar.gz $SERVICE_NAME
-			rm -rf $SERVICE_NAME
-		fi
+        if [ -d $SERVICE_NAME ]
+        then
+            echo Backup service
+              [[ -d $SERVICE_DIR/$serviceBackupDir ]] || mkdir $SERVICE_DIR/$serviceBackupDir
+            create_time=`date -r $SERVICE_NAME +"%Y%m%d_%H%M%S"`
+            tar czf $serviceBackupDir/"$SERVICE_NAME"_"$create_time".tar.gz $SERVICE_NAME
+            rm -rf $SERVICE_NAME
+        fi
 
-    	echo Extract $ZIP_FILE_NAME
-    	tar xzf $ZIP_FILE_NAME
-    	echo Remove $ZIP_FILE_NAME
+        echo Extract $ZIP_FILE_NAME
+        tar xzf $ZIP_FILE_NAME
+        echo Remove $ZIP_FILE_NAME
       rm $ZIP_FILE_NAME
 fi
 
 echo Start service $SERVICE_NAME
 cd $SERVICE_DIR/$SERVICE_NAME
-nohup ./$SERVICE_NAME $SERVICE_PARAM >> log_$SERVICE_NAME.log 2>&1 &
+nohup ./$SERVICE_NAME $SERVICE_PARAM $4 $5>> log_$SERVICE_NAME.log 2>&1 &
