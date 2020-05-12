a6911ae87c14d2569c09c04e6efc7a7377b54f7c
Stephen <liwp.stephen@gmail.com>
2020-05-12 16:39:21 +0700

    update command to create ftp user

--- linux/add_ftpuser.sh
+++ linux/add_ftpuser.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-mkdir ~/ftp && chmod 550 ~/ftp
-mkdir ~/ftp/upload && chmod 750 ~/ftp/upload
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo 'Please run the script as root'
+  exit
+fi
+
+if [ "$1" != "" ]; then
+  useradd $1
+  su $1 -c "mkdir -p ~/ftp/upload"
+  su $1 -c "chmod 550 ~/ftp"
+  su $1 -c "chmod 750 ~/ftp/upload"
+
+  echo "$1" >> /etc/vsftpd/user_list
+  usermod $1 -s /bin/ftponly
+
+  echo "User $1 created, please set the password"
+else
+  echo "The username is not specified"
+fi
